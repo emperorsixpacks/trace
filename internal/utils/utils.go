cdbe3f3b4fd3ffa44f3587ec2f5e9d8f2c32a645
Andrew David <andrewdvd10@gmail.com>
2026-05-26 02:16:47 +0100

    refactor: remove yaml/json parsing from utils

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,12 +1,5 @@
 package utils
 
-import "github.com/emperorsixpacks/trace/model"
-
-var reader = map[model.FileFormat]func(string) ([]byte, error){
-	model.YAML: loadYamlFile,
-	model.JSON: loadJsonFile,
-}
-
-func LoadFile(path string, filetype model.FileFormat) error {
-	return reader[filetype](path)
+func LoadFile(path string) ([]byte, error) {
+	return readFile(path)
 }
--- internal/utils/utils_.go
+++ internal/utils/utils_.go
@@ -1,11 +1,8 @@
 package utils
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
-
-	"gopkg.in/yaml.v3"
 )
 
 func readFile(path string) ([]byte, error) {
@@ -14,30 +11,6 @@ func readFile(path string) ([]byte, error) {
 		if os.IsNotExist(err) {
 			return nil, fmt.Errorf("File %s not found", path)
 		}
-		return nil, err
-	}
-	return out, nil
-}
-
-func loadYamlFile(path string) ([]byte, error) {
-	out, err := readFile(path)
-	if err != nil {
-		return nil, err
-	}
-	err = yaml.Unmarshal(out, &out)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func loadJsonFile(path string) ([]byte, error) {
-	out, err := readFile(path)
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(out, &out)
-	if err != nil {
 		return nil, err
 	}
 	return out, nil
