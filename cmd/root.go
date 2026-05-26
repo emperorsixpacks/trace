ec03ebda1fafe8aa2ca981bf89338a2f2c63d051
Andrew David <andrewdvd10@gmail.com>
2026-05-26 01:37:15 +0100

    refactor: replace old main entry with cobra root command

--- cmd/root.go
+++ cmd/root.go
@@ -8,7 +8,7 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "trace_cli",
+	Use:   "trace",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
@@ -16,9 +16,9 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("trace_cli called")
-	},
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
