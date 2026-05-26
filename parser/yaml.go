f36a8c70071ed6c14a7401b76cb32edd479724f5
Andrew David <andrewdvd10@gmail.com>
2026-05-26 02:16:53 +0100

    feat: add YAML parser

--- parser/yaml.go
+++ parser/yaml.go
@@ -1,1 +1,15 @@
 package parser
+
+import (
+	"github.com/emperorsixpacks/trace/model"
+	"gopkg.in/yaml.v3"
+)
+
+func ParseYaml(data []byte) (*model.Flow, error) {
+	var flow model.Flow
+	err := yaml.Unmarshal(data, &flow)
+	if err != nil {
+		return nil, err
+	}
+	return &flow, nil
+}
